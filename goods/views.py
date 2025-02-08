17620fae4d90cba46f16fd48fd75c00baeeaa3c4
Roman <cisinskijroman@gmail.com>
2025-02-08 14:31:39 +0200

    Slug  convertor for product

--- goods/views.py
+++ goods/views.py
@@ -14,5 +14,13 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slag):
+
+    product = Products.objects.get(slag=product_slag)
+
+    context   = {
+        'product': product
+
+
+    }
+    return render(request, "goods/product.html", context)
